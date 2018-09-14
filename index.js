7a80dc9bd5472acce238fc5d92db3961a3f3f21e
3103ski <bryan@uppermesa.com>
2018-09-13 19:21:06 -0700

    Sets up empty JS IIFEs

---
+++ index.js
@@ -0,0 +1,36 @@
+// ***********************
+// Map Controller Module
+// ***********************
+
+var mapController = (function() {
+
+})();
+
+
+// ***********************
+// Data Input Module
+// ***********************
+
+
+var dataModule = (function() {
+
+})();
+
+
+// ***********************
+// UI Module
+// ***********************
+
+var UIModule = (function() {
+
+})();
+
+
+// ***********************
+// Controller Module
+// ***********************
+
+var controllerModule = (function(mapCtrl, dataCtrl, UICtrl) {
+
+
+})(mapController, dataModule, UIModule);
