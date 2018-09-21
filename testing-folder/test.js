30da14f3c84531bed0e5f5f24e2936f6d272d9be
3103ski <bryan@uppermesa.com>
2018-09-20 17:13:28 -0700

    updates testing map to show input address on map

--- testing-folder/test.js
+++ testing-folder/test.js
@@ -3,6 +3,17 @@
 function showResult(result) {
     document.getElementById('latitude').value = result.geometry.location.lat();
     document.getElementById('longitude').value = result.geometry.location.lng();
+
+    var inputLat, inputLong;
+
+    inputLat = result.geometry.location.lat();
+    inputLong = result.geometry.location.lng();
+
+    initMap(inputLat, inputLong);
+
+    // return inputLat, inputLong;
+
+
 }
 
 function getLatitudeLongitude(callback, address) {
@@ -25,5 +36,30 @@ var button = document.getElementById('btn');
 
 button.addEventListener("click", function() {
     var address = document.getElementById('address').value;
-    getLatitudeLongitude(showResult, address)
+    getLatitudeLongitude(showResult, address);
 });
+
+
+
+
+// Initialize and add the map
+function initMap(lat, lng) {
+    // The location of Uluru
+    var uluru = {
+        // lat: 47.2981173,
+        // lng: -122.3882006
+        lat: parseInt(lat),
+        lng: parseInt(lng)
+    };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 8,
+            center: uluru
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
